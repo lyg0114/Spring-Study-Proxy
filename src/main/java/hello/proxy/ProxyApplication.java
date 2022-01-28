abaa503c29cc8287355603d49f153a748fa80ddc
yglee <yglee@ksmartpia.com>
2022-01-28 23:23:46 +0900

    프록시 팩토리 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,8 +1,7 @@
 package hello.proxy;
 
-import hello.proxy.config.v1_proxy.ConcreateProxyConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_dynamicproxy.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_dynamicproxy.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -15,7 +14,9 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreateProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
---
+++ src/main/java/hello/proxy/config/v3_dynamicproxy/ProxyFactoryConfigV1.java
@@ -0,0 +1,67 @@
+package hello.proxy.config.v3_dynamicproxy;
+
+import hello.proxy.app.v1.*;
+import hello.proxy.config.v3_dynamicproxy.advice.LogTraceAdvice;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class ProxyFactoryConfigV1 {
+
+    @Bean
+    public OrderControllerV1 orderControllerV1(LogTrace logTrace){
+
+        OrderControllerV1 orderController = new OrderControllerV1Impl(orderServiceV1(logTrace));
+        ProxyFactory factory = new ProxyFactory(orderController);
+        factory.addAdvisor(getAdvisor(logTrace));
+        OrderControllerV1 proxy = (OrderControllerV1) factory.getProxy();
+        log.info("ProxyFactory proxy={}, target={}", proxy.getClass(), orderController.getClass());
+        return proxy;
+
+    }
+
+
+    @Bean
+    public OrderServiceV1 orderServiceV1(LogTrace logTrace){
+
+        OrderServiceV1 orderService = new OrderServiceV1Impl(orderRepositroyV1(logTrace));
+        ProxyFactory factory = new ProxyFactory(orderService);
+        factory.addAdvisor(getAdvisor(logTrace));
+        OrderServiceV1 proxy = (OrderServiceV1) factory.getProxy();
+        log.info("ProxyFactory proxy={}, target={}", proxy.getClass(), orderService.getClass());
+        return proxy;
+
+    }
+
+    @Bean
+    public OrderRepositroyV1 orderRepositroyV1(LogTrace logTrace){
+
+        OrderRepositoryV1Impl orderRepository = new OrderRepositoryV1Impl();
+        ProxyFactory factory = new ProxyFactory(orderRepository);
+        factory.addAdvisor(getAdvisor(logTrace));
+        OrderRepositroyV1 proxy = (OrderRepositroyV1) factory.getProxy();
+        log.info("ProxyFactory proxy={}, target={}", proxy.getClass(), orderRepository.getClass());
+
+        return proxy;
+
+    }
+
+    private Advisor getAdvisor(LogTrace logTrace) {
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("request*", "order*", "save*");
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+
+        return new DefaultPointcutAdvisor(pointcut, advice);
+
+    }
+
+
+}
