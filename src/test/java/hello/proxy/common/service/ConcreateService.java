21de851a8d5a079d2c2ee7bdc380c81086caedf3
yglee <yglee@ksmartpia.com>
2022-01-28 22:01:24 +0900

    스프링이 제공하는 포인트컷

---
+++ src/test/java/hello/proxy/common/service/ConcreateService.java
@@ -0,0 +1,12 @@
+package hello.proxy.common.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ConcreateService {
+
+    public void call(){
+       log.info("ConcreateService 호출");
+    }
+
+}
